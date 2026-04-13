cd019c8ca46aecd5b4828a0d2d7212355a616fe4
Gonçalo Rebelo de Andrade <goncaloandrade@Mac.Home>
2026-04-14 00:55:43 +0100

    Fix: Revert base path to /pickwell/ to solve white screen error

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',  // <-- changed from '/pickwell/'
+  base: '/pickwell/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
