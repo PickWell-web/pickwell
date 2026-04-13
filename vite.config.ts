8e5e589cbf45c11474d9458dfd498cae2ad5c954
PickWell-web <geral@pickwell.pt>
2026-04-14 00:33:47 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/pickwell/',
+  base: '/',  // <-- changed from '/pickwell/'
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
