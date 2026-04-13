c1049f2dbcd08eb6f2150b255d7a544d3b1a3fbf
Gonçalo Rebelo de Andrade <goncaloandrade@Mac.Home>
2026-04-14 00:21:09 +0100

    Revert image to webpage.jpeg and set default language to PT

--- src/components/CustomizedExperience.tsx
+++ src/components/CustomizedExperience.tsx
@@ -84,7 +84,7 @@ export default function CustomizedExperience() {
           >
             <div className="relative w-full max-w-[320px] rounded-[2.5rem] overflow-hidden shadow-2xl border-4 border-white">
               <img 
-                src="webpage_iphone.png" 
+                src="webpage.jpeg" 
                 alt="PickWell App" 
                 className="w-full h-auto block"
               />
